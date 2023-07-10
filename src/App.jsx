6e0c357f07c522ded9db82f07ef0612b75a26c21
EiSandyPhyo <eisandyphyo345@gmail.com>
2023-07-10 21:34:40 +0630

    12 commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,27 @@ import Header from './Header'
 const App = () => {
   console.log(navigator.connection.effectiveType);
 
-  const [loading, setLoading] = useState(true);
-  const preloading = document.getElementById('preloading');
-  if(preloading){
-    setTimeout(()=>{
-      preloading.style.display = 'none';
-      setLoading(false);
-    },2000)
-  }
+  // const [loading, setLoading] = useState(true);
+  // const preloading = document.getElementById('preloading');
+  // if(preloading){
+  //   setTimeout(()=>{
+  //     preloading.style.display = 'none';
+  //     setLoading(false);
+  //   },2000)
+  // }
   return (
-    !loading && (
-      <>
+    // !loading && (
+    //   <>
+    //   <Header>
+    //   <a href="#firstPage" className=" mx-5 hover:text-[#C44D58]">Section Oneee</a>
+    //   <a href="#secondPage" className=" mx-5  hover:text-[#C44D58]">Section Twooo</a>
+    //   <a href="#thirdPage" className=" mx-5  hover:text-[#C44D58]">Section Threeee</a>
+    //   </Header>
+    //   <SectionContainer/>
+    // </>
+    // )
+
+    <>
       <Header>
       <a href="#firstPage" className=" mx-5 hover:text-[#C44D58]">Section Oneee</a>
       <a href="#secondPage" className=" mx-5  hover:text-[#C44D58]">Section Twooo</a>
@@ -23,7 +33,6 @@ const App = () => {
       </Header>
       <SectionContainer/>
     </>
-    )
   )
 }
 
