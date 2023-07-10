5d45ca1d248babbb13efed3c879b5245c1c27b5e
EiSandyPhyo <eisandyphyo345@gmail.com>
2023-07-10 15:45:40 +0630

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+import SectionContainer from './SectionContainer'
+import Header from './Header'
+
+const App = () => {
+  console.log(navigator.connection.effectiveType);
+
+  const [loading, setLoading] = useState(true);
+  const preloading = document.getElementById('preloading');
+  if(preloading){
+    setTimeout(()=>{
+      preloading.style.display = 'none';
+      setLoading(false);
+    },2000)
+  }
+  return (
+    !loading && (
+      <>
+      <Header>
+      <a href="#firstPage" className=" mx-5 hover:text-[#C44D58]">Section Oneee</a>
+      <a href="#secondPage" className=" mx-5  hover:text-[#C44D58]">Section Twooo</a>
+      <a href="#thirdPage" className=" mx-5  hover:text-[#C44D58]">Section Threeee</a>
+      </Header>
+      <SectionContainer/>
+    </>
+    )
+  )
+}
+
+export default App
---
+++ src/SectionContainer.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import ReactFullpage from "@fullpage/react-fullpage";
+import First from './Components/First';
+import Second from './Components/Second';
+import Third from './Components/Third';
+
+// const fullpage = document.getElementById('fullpage');
+// fullpage.style.transitionTimingFunction = 'cubic-bezier(.55,.085,0,.99)'
+
+const SectionContainer = () => {
+    const anchors = ["firstPage", "secondPage", "thirdPage"];
+
+const FullpageWrapper = () => (
+  <ReactFullpage
+  scrollingSpeed = {1000}
+    anchors={anchors}
+    navigation
+    navigationTooltips={anchors}
+    navigat
+    sectionsColor={["#3c3b5a", "#32324c", "#3c3b5a"]}
+    pp-easing='.pp-easing'
+    onLeave={(origin, destination, direction) => {
+      console.log("onLeave event", { origin, destination, direction });
+    }}
+    render={({ state, fullpageApi }) => {
+    //   console.log("render prop change", state, fullpageApi); // eslint-disable-line no-console
+
+      return (
+        <div className='pp-easing pp-section'>
+          <div className="section">
+            <First/>
+          </div>
+          <div className="section">
+            <Second/>
+          </div>
+          <div className="section">
+            <Third/>
+          </div>
+        </div>
+      );
+    }}
+  />
+);
+  return (
+    <FullpageWrapper/>
+  )
+}
+
+export default SectionContainer
